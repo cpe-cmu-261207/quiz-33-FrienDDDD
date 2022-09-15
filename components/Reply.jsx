71aad1c74d79b3b66626a8c984814e601299bcce
FrienDDDD <friendjunk@gmail.com>
2022-09-15 14:50:37 +0700

    update reply

--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export default function Reply(props) {
-  return <div></div>;
+  return (
+    <div className="d-flex gap-2 my-2 ps-5">
+      <img
+        src={props.imgpath}
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className="rounded rounded-3 p-2"
+        style={{ backgroundColor: "#3A3B3C" }}
+      >
+        <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+          {props.name}
+        </span>
+        <br />
+        <span style={{ color: "#E4E6EB" }}>{props.reply}</span>
+        <div
+          className={
+            props.like > 0 ? "d-flex align-items-center gap-1" : "d-none"
+          }
+        >
+          <img src="/like.svg" width={20}></img>
+          <span style={{ color: "#B0B3B8" }}>{props.like} คน</span>
+        </div>
+      </div>
+    </div>
+  );
 }
