06e8b2263dd382a8258e2f5b7761fdd171125500
FrienDDDD <friendjunk@gmail.com>
2022-09-15 14:50:19 +0700

    update comment

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,9 +5,41 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.imgpath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {props.name}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{props.comment}</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>{props.like} คน</span>
+          </div>
+        </div>
+      </div>
 
-      {/*render Reply here... */}
+      {props.replie.map((x, i) => {
+        return (
+          <Reply
+            key={i}
+            name={x.username}
+            like={x.likeNum}
+            imgpath={x.userImagePath}
+            reply={x.replyText}
+          />
+        );
+      })}
     </div>
   );
 }
